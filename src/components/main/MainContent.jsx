f2de46b7b450884ae6ae2d46d67ace9443c23975
JeffTunner <abhinavkaushik05@gmail.com>
2025-09-21 00:43:52 +0530

    Day 8- Search Container responsively restyled

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -3,7 +3,7 @@ import ContentContainer from "./ContentContainer.jsx";
 
 function MainContent() {
     return (
-        <main className="lg:px-28 lg:pb-20 flex flex-col gap-8 lg:gap-12">
+        <main className="">
             <SearchContainer />
             <ContentContainer />
         </main>
--- src/components/main/SearchContainer.jsx
+++ src/components/main/SearchContainer.jsx
@@ -4,15 +4,13 @@ import SearchIcon from "../../assets/images/icon-search.svg";
 function SearchContainer() {
 
     return (
-        <section className="md:flex-row items-center justify-center gap-3 lg:gap-4 flex flex-col">
-            <div className="flex gap-4 bg-Neutral-800 px-6 py-4 w-screen md:w-full lg:max-w-[526px] rounded-xl cursor-pointer hover:bg-Neutral-700">
+        <section className="md:flex md:items-center md:justify-center gap-3 lg:gap-4 grid">
+            <div className="flex bg-Neutral-800 px-6 py-4 gap-4 items-center justify-center rounded-xl w-full md:max-w-[590px] lg:max-w-[526px] hover:bg-Neutral-700">
                 <img src={SearchIcon} alt="search-icon"/>
-                <p className="text-Neutral-200 font-dm font-medium text-xl cursor-pointer">
-                    Search for a place...
-                </p>
+                <input type="text" placeholder="Search for a place..." className="font-dm font-medium text-Neutral-200 text-xl leading-[120%] w-full border-none"/>
             </div>
             <div>
-                <button className="bg-LightBlue text-xl font-dm font-medium text-Neutral-0 px-6 py-4 rounded-xl w-screen lg:w-full hover:bg-DarkBlue">
+                <button className="bg-LightBlue h-14 flex items-center justify-center text-xl font-dm font-medium text-Neutral-0 px-6 py-4 rounded-xl w-full cursor-pointer hover:bg-DarkBlue">
                     Search
                 </button>
             </div>
