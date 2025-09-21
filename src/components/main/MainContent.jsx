545c5633d164428a6fddebb5a4233092e5723a07
JeffTunner <abhinavkaushik05@gmail.com>
2025-09-21 12:52:05 +0530

    Day 9- WeatherInfo resposnively restyled

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -3,7 +3,7 @@ import ContentContainer from "./ContentContainer.jsx";
 
 function MainContent() {
     return (
-        <main className="">
+        <main className="flex flex-col gap-8 lg:gap-12">
             <SearchContainer />
             <ContentContainer />
         </main>
--- src/components/main/WeatherInfo.jsx
+++ src/components/main/WeatherInfo.jsx
@@ -1,24 +1,41 @@
 import BgLarge from "../../assets/images/bg-today-large.svg";
+import BgSmall from "../../assets/images/bg-today-small.svg";
 import sunIcon from "../../assets/images/icon-sunny.webp";
+import { useState, useEffect } from "react";
 
 function WeatherInfo() {
 
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    },[]);
+
     return (
+        <>
         <section className="flex flex-col items-center">
-            <div className="relative h-[286px] w-screen lg:max-w-[800px]"
-            style={{backgroundImage: `url(${BgLarge})`}}>                
-                <div className="absolute inset-0 flex flex-col justify-center rounded-[20px] gap-4 lg:flex-row items-center text-Neutral-0 px-6 lg:py-20 lg:justify-between">
-                    <div className="flex flex-col gap-3 font-dm text-Neutral-0">
-                        <h1 className="font-bold text-[28px]">Berlin, Germany</h1>
-                        <p className="font-medium text-[18px]">Tuesday, Aug 5, 2025</p>
+            <div className="relative h-[286px] w-full bg-cover bg-center rounded-[20px]"
+            style={{backgroundImage: width > 768 ? `url(${BgLarge})` : `url(${BgSmall})`}}>
+                <div className="absolute inset-0 flex flex-col justify-center items-center px-6 md:flex-row md:justify-between">
+                    <div className="flex flex-col gap-3">
+                        <h1 className="font-dm font-bold text-Neutral-0 text-[28px] leading-[120%]">Berlin, Germany</h1>
+                        <p className="font-dm font-medium text-Neutral-200 text-[18px] leading-[120%]">Tuesday, Aug 5, 2025</p>
                     </div>
                     <div className="flex items-center gap-5">
                         <img src={sunIcon} alt="sunIcon" className="w-[120px] h-[120px]"/>
-                        <h2 className="font-dm font-semibold italic text-8xl tracking-[-2px]">20°</h2>
+                        <h2 className="font-dm font-semibold italic text-Neutral-0 text-8xl tracking-[-2px] leading-[100%]">20°</h2>
                     </div>
                 </div>
             </div>
         </section>
+        </>
     );
 }
 export default WeatherInfo;
