8220b3f37d96a58c8734075c575598b82ff5160e
JeffTunner <abhinavkaushik05@gmail.com>
2025-10-01 12:07:42 +0530

    Day 16- Daily forecast also intefrated with context and conditional rendering too

--- src/components/main/DailyForecast.jsx
+++ src/components/main/DailyForecast.jsx
@@ -5,8 +5,32 @@ import pCloudyIcon from "../../assets/images/icon-partly-cloudy.webp";
 import stormIcon from "../../assets/images/icon-storm.webp";
 import snowIcon from "../../assets/images/icon-snow.webp";
 import fogIcon from "../../assets/images/icon-fog.webp";
+import overcastIcon from "../../assets/images/icon-overcast.webp";
+import { useWeather } from "../../context/WeatherContext.jsx";
+import { useState, useEffect } from "react";
 
 function DailyForecast() {
+
+    const {weatherData} = useWeather();
+
+    function getWeatherIcon(code) {
+    if (code === 0 || code === 1) return sunIcon;
+    if (code === 2) return pCloudyIcon;
+    if (code === 3) return overcastIcon;
+    if (code === 45 || code === 48) return fogIcon;
+    if (code >= 51 && code <= 57) return drizzleIcon;
+    if (code >= 61 && code <= 67) return rainIcon;
+    if (code >= 71 && code <= 77) return snowIcon;
+    if (code >= 80 && code <= 82) return rainIcon;
+    if (code === 85 || code === 86) return snowIcon;
+    if (code >= 95 && code <= 99) return stormIcon;
+    return sunIcon;
+    }
+
+    function getDayName(dateString) {
+        const date = new Date(dateString);
+        return date.toLocaleDateString("en-US", {weekday: "short"});
+    }
 
     return(
         <section className="flex flex-col gap-5">
@@ -14,8 +38,22 @@ function DailyForecast() {
                 <h1 className="text-Neutral-0 font-dm font-semibold text-xl leading-[120%]">Daily Forecast</h1>
             </div>
             <div className="grid grid-cols-3 gap-4 md:grid-cols-7">
-
-                <div className="flex flex-col items-center bg-Neutral-800 border border-Neutral-600 rounded-xl px-2.5 py-4 gap-4">
+                {weatherData ? (weatherData?.daily?.time.map((date, i) => (
+                    <div key={i} className="flex flex-col items-center bg-Neutral-800 border border-Neutral-600 rounded-xl px-2.5 py-4 gap-4">
+                        <h2 className="font-dm font-medium text-lg leading-[120%] text-Neutral-0">{getDayName(date)}</h2>
+                        <img src={getWeatherIcon(weatherData.daily.weather_code[i])} alt="rainIcon" className="w-[60px] h-[60px]"/>
+                        <span className="flex gap-[33.57px]">
+                            <p className="text-Neutral-0 font-dm font-medium text-[16px] leading-[120%]">
+                                {Math.round(weatherData.daily.temperature_2m_max[i])}°
+                            </p>
+                            <p className="text-Neutral-200 font-dm font-medium text-[16px] leading-[120%]">
+                                {Math.round(weatherData.daily.temperature_2m_min[i])}°
+                            </p>
+                        </span>
+                    </div>
+                ))): 
+                (<>
+                                <div className="flex flex-col items-center bg-Neutral-800 border border-Neutral-600 rounded-xl px-2.5 py-4 gap-4">
                     <h2 className="font-dm font-medium text-lg leading-[120%] text-Neutral-0">Tue</h2>
                     <img src={rainIcon} alt="rainIcon" className="w-[60px] h-[60px]"/>
                     <span className="flex gap-[33.57px]">
@@ -77,6 +115,7 @@ function DailyForecast() {
                         <p className="text-Neutral-200 font-dm font-medium text-[16px] leading-[120%]">15°</p>
                     </span>
                 </div>
+                </>)}
 
             </div>
         </section>
