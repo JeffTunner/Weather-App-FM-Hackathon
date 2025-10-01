35229dd6e18cbee6c199f968cfca583a57ea38b1
JeffTunner <abhinavkaushik05@gmail.com>
2025-10-01 11:28:50 +0530

    Day 16- Current weather detail aslo integrated with context

--- src/components/main/WeatherDetail.jsx
+++ src/components/main/WeatherDetail.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
+import { useWeather } from "../../context/WeatherContext.jsx";
 
 function WeatherDetail() {
+
+    const {weatherData, windUnit, precipitationUnit} = useWeather();
 
     return (
         <section className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-5 lg:gap-6">
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Feels Like</h1>
-                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">18°</p>
+                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
+                    {weatherData ? `${weatherData?.current?.apparent_temperature}°` : "18°"}
+                </p>
             </div>
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Humidity</h1>
-                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">46%</p>
+                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
+                    {weatherData ? `${weatherData?.current?.relative_humidity_2m}%` : "46%"}
+                </p>
             </div>
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Wind</h1>
-                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">14 km/h</p>
+                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
+                    {weatherData ? `${weatherData?.current?.wind_speed_10m}` : "14"} {windUnit}
+                </p>
             </div>
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Precipitation</h1>
-                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">0 mm</p>
+                <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
+                    {weatherData ? `${weatherData?.current?.precipitation}` : "0"} {precipitationUnit}
+                </p>
             </div>
         </section>
     );
