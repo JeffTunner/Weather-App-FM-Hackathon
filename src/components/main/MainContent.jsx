81f5422cbd2fc439b9cdc1b1614d95bdf5093ba1
JeffTunner <abhinavkaushik05@gmail.com>
2025-10-03 19:28:13 +0530

    Day 18- GeoLocation api error state conditionally loaded

--- src/components/main/MainContent.jsx
+++ src/components/main/MainContent.jsx
@@ -1,11 +1,26 @@
 import SearchContainer from "./SearchContainer.jsx";
 import ContentContainer from "./ContentContainer.jsx";
+import { useWeather } from "../../context/WeatherContext.jsx";
 
 function MainContent() {
+
+    const {locationError} = useWeather();
+
     return (
         <main className="flex flex-col gap-8 lg:gap-12">
-            <SearchContainer />
-            <ContentContainer />
+            {!locationError ? (
+                <>
+                    <SearchContainer />
+                    <ContentContainer />
+                </>
+            ) : (
+                <section className="flex flex-col h-screen gap-12">
+                    <SearchContainer />
+                    <div className="font-dm font-bold text-Neutral-0 text-[28px] text-center leading-[120%]">
+                        {locationError}
+                    </div>
+                </section>
+            )}
         </main>
     );
 }
