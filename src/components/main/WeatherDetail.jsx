ac662a95c297535bbe311e00fa68b2e60b7332a7
JeffTunner <abhinavkaushik05@gmail.com>
2025-10-05 22:30:52 +0530

    day 19

--- src/components/main/WeatherDetail.jsx
+++ src/components/main/WeatherDetail.jsx
@@ -3,32 +3,32 @@ import { useWeather } from "../../context/WeatherContext.jsx";
 
 function WeatherDetail() {
 
-    const {weatherData, windUnit, precipitationUnit} = useWeather();
+    const {weatherData, windUnit, precipitationUnit, loading} = useWeather();
 
     return (
         <section className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-5 lg:gap-6">
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Feels Like</h1>
                 <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
-                    {weatherData ? `${weatherData?.current?.apparent_temperature}°` : "18°"}
+                    {loading ? "-" : weatherData ? `${weatherData?.current?.apparent_temperature}°` : "18°"}
                 </p>
             </div>
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Humidity</h1>
                 <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
-                    {weatherData ? `${weatherData?.current?.relative_humidity_2m}%` : "46%"}
+                    {loading ? "-" : weatherData ? `${weatherData?.current?.relative_humidity_2m}%` : "46%"}
                 </p>
             </div>
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Wind</h1>
                 <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
-                    {weatherData ? `${weatherData?.current?.wind_speed_10m}` : "14"} {windUnit}
+                    {loading ? "-" : weatherData ? `${weatherData?.current?.wind_speed_10m}` : "14"} {!loading && windUnit}
                 </p>
             </div>
             <div className="bg-Neutral-800 p-5 rounded-xl border border-Neutral-600 flex flex-col gap-6">
                 <h1 className="font-dm font-medium text-lg leading-[120%] text-Neutral-200">Precipitation</h1>
                 <p className="font-dm font-light text-[32px] leading-[100%] text-Neutral-0">
-                    {weatherData ? `${weatherData?.current?.precipitation}` : "0"} {precipitationUnit}
+                    {loading ? "-" : weatherData ? `${weatherData?.current?.precipitation}` : "0"} {!loading && precipitationUnit}
                 </p>
             </div>
         </section>
